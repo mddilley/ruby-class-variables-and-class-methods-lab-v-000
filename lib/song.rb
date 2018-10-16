0aaa78301baf16601ec97504e6e0a3217251c54d
Mike Dilley <mike.dilley@gmail.com>
2018-10-16 20:51:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,7 @@ class Song
   def self.genre_count
     genre_count = {}
     self.genres.each do |genre|
+      binding.pry
       genre_count[genre] = ([genre_count[genre] == nil ? 1 : genre_count[genre] += 1])  
     end
     genre_count
